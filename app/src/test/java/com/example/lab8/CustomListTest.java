1fa4cff737f7513143f8147e1e867b11b9111c60
emajiid <125160989+emajiid@users.noreply.github.com>
2024-10-24 17:32:25 -0600

    First commit

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,21 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    public CustomList mockCityList(){
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        CustomList list = mockCityList();
+        int expectedCountBeforeAdd = 0;
+        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count should have been zero");
+        City cityToAdd = new City("Red Deer", "AB");
+        list.add(cityToAdd);
+
+        int expectedCountAfterAdd = 1;
+        assertEquals(expectedCountAfterAdd, list.getCount(), "City was not added to the list");
+    }
 
 }
